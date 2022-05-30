5faabda80d91ac983bce18d1b594246592dc0e40
Braden-Smith21 <braden.smith04292002@gmail.com>
2022-05-30 18:59:12 -0500

    First Commit For app

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Rich Text file (*.rtf)|*.rtf";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, newNoteBox.Text);
+            }
+
+        }
+
+        private void toggleBullets_Click(object sender, RoutedEventArgs e)
+        {
+            EditingCommands.ToggleBullets.Execute(null, newNoteBox);
+        }
+
+        private void underLine_Click(object sender, RoutedEventArgs e)
+        {
+           EditingCommands.ToggleUnderline.Execute(null, newNoteBox);  
+        }
+
+        private void toggleNumerals_Click(object sender, RoutedEventArgs e)
+        {
+            EditingCommands.ToggleNumbering.Execute(null, newNoteBox);
+        }
+
+        private void Bold_Click(object sender, RoutedEventArgs e)
+        {
+            EditingCommands.ToggleBold.Execute(null, newNoteBox);
+        }
+
+        private void Italics_Click(object sender, RoutedEventArgs e)
+        {
+            EditingCommands.ToggleItalic.Execute(null, newNoteBox);
+        }
+    }
+}
+
+
+
